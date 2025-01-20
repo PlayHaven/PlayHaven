62419a9a92cfcf67a8f86a3b5aa0ad0795360b3e
zamieabdullah <zamieabdullah0@gmail.com>
2025-01-20 16:38:53 -0500

    new origin for new domain

--- app/__init__.py
+++ app/__init__.py
@@ -23,7 +23,9 @@ def create_app(config_class=Config):
              "origins": [
                  "http://localhost:3000",
                  "http://localhost:5001",
-                 "https://playhaven-fe.onrender.com"
+                 "https://playhaven-fe.onrender.com",
+                 "https://www.playhaven.app",
+                 "https://playhaven.app"
              ],
              "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
              "allow_headers": ["Content-Type", "Authorization", "X-CSRF-TOKEN"],
